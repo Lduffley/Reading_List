73943bf0a926953614335cf340301b65a26c9aed
Logan Duffley <Lduffley12@gmail.com>
2020-02-03 12:36:55 -0600

    Added more context providers

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import NavBar from './components/navbar';
 import BookList from './components/BookList';
 import ThemeContextProvider from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext';
 
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <NavBar/>
-        <BookList/>
+        <AuthContextProvider>
+          <NavBar/>
+          <BookList/>
+          <ThemeToggle/>
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
