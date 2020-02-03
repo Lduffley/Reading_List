91abf5b4b721d6dd44af6e80ddcfa6ae4161d529
Logan Duffley <Lduffley12@gmail.com>
2020-02-03 12:42:25 -0600

    Converted Class to Func Comp (useContext) hook

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,23 +1,22 @@
-import React, { Component, } from "react";
+import React, { useContext } from "react";
 import {ThemeContext} from "../contexts/ThemeContext"
 
-class BookList extends Component {
-    static contextType = ThemeContext;
-    render() { 
-        const {isLightTheme, light, dark} = this.context
-        const theme = isLightTheme ? light : dark;
-        return (
-            <div className='book-list' style={{color: theme.syntax, background: theme.bg}}>
-            <ul>
-                <li style={{background: theme.ui}}>Harry Potter</li>
-                <li style={{background: theme.ui}}>Lord of the Rings</li>
-                <li style={{background: theme.ui}}>Magic Treehouse</li>
-            </ul>
-            </div>
-          );
-    }
+
+function BookList() {
+    const {isLightTheme, light, dark} = useContext(ThemeContext)
+    const theme = isLightTheme ? light : dark;
+    return (
+        <div className='book-list' style={{color: theme.syntax, background: theme.bg}}>
+        <ul>
+            <li style={{background: theme.ui}}>Harry Potter</li>
+            <li style={{background: theme.ui}}>Lord of the Rings</li>
+            <li style={{background: theme.ui}}>Magic Treehouse</li>
+        </ul>
+        </div>
+      );
 }
-
-
  
 export default BookList;
+
+ 
+
