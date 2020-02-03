1c90053d8ec25e8fc9cc0aaaac2baa98fedb02d1
Lduffley <53620669+Lduffley@users.noreply.github.com>
2020-02-02 22:46:15 -0600

    Update BookList.js

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -11,7 +11,7 @@ class BookList extends Component {
             <ul>
                 <li style={{background: theme.ui}}>Harry Potter</li>
                 <li style={{background: theme.ui}}>Lord of the Rings</li>
-                <li style={{background: theme.ui}}>Of Mice of Men</li>
+                <li style={{background: theme.ui}}></li>
             </ul>
             </div>
           );
